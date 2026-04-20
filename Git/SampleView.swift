04ff263d3ad0cb00bc84769b7c095449ffa45990
BT_C09_21 <mobileappdev@bths.edu>
2026-04-20 13:34:59 -0400

    added the SampleView file

---
+++ Git/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  Git
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("look at this simple view")
+    }
+}
+
+#Preview {
+    SampleView()
+}
